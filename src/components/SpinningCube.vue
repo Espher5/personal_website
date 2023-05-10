2eae4c14f336cc24cc5cf75ba652468a8ae4c2fd
Michelangelo Esposito <michelangeloesposito24@gmail.com>
2023-05-10 18:33:37 +0200

    .

--- src/components/SpinningCube.vue
+++ src/components/SpinningCube.vue
@@ -27,70 +27,70 @@ export default {
     scene.add(particles[0]);
 
     const animate = () => {
-      requestAnimationFrame(animate);
-      camera.position.z -= 0.1;
-    
-      if (camera.position.z <= tunnelDepth - (particleBatchDepth * 3/4) ) {
-        //Update total depth
-        tunnelDepth += particleBatchDepth;
+        requestAnimationFrame(animate);
+        camera.position.z -= 0.1;
+        
+        if (camera.position.z <= tunnelDepth - (particleBatchDepth * 3/4) ) {
+            //Update total depth
+            tunnelDepth += particleBatchDepth;
 
-        //Generate offset for new particles
-        offset = new THREE.Vector3(0, 0, tunnelDepth);
-        particles[nextParticlesIndex] = createParticles(offset);
-        scene.add(particles[nextParticlesIndex]);
+            //Generate offset for new particles
+            offset = new THREE.Vector3(0, 0, tunnelDepth);
+            particles[nextParticlesIndex] = createParticles(offset);
+            scene.add(particles[nextParticlesIndex]);
 
-        // Update index and remove old particles
-        nextParticlesIndex = (nextParticlesIndex + 1 ) % 2
-        scene.remove(particles[nextParticlesIndex])
-      }
+            // Update index and remove old particles
+            nextParticlesIndex = (nextParticlesIndex + 1 ) % 2
+            scene.remove(particles[nextParticlesIndex])
+        }
 
-      renderer.render(scene, camera);
+        renderer.render(scene, camera);
     };
     animate();
 
     function createParticles(offset) {
-      const geometry = new THREE.BufferGeometry();
-      const positions = createPositions(offset);
-      const colors = createColors();
-      geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-      geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-      const material = new THREE.PointsMaterial({ size: 0.1, vertexColors: true });
-      return new THREE.Points(geometry, material);
+        const geometry = new THREE.BufferGeometry();
+        const positions = createPositions(offset);
+        const colors = createColors();
+        geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+        geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+        const material = new THREE.PointsMaterial({ size: 0.1, vertexColors: true });
+        return new THREE.Points(geometry, material);
     }
 
     function createPositions(offset) {
-      const positions = new Float32Array(10000 * 3);
-      for (let i = 0; i < 10000; i++) {
-        const pos = new THREE.Vector3(
-          Math.random() * 80 - 40,
-          Math.random() * 80 - 40,
-          Math.random() * 400 + offset.z
-        );
-        positions[i * 3] = pos.x;
-        positions[i * 3 + 1] = pos.y;
-        positions[i * 3 + 2] = pos.z;
-      }
-      return positions;
+        const positions = new Float32Array(10000 * 3);
+        for (let i = 0; i < 10000; i++) {
+            const pos = new THREE.Vector3(
+            Math.random() * 80 - 40,
+            Math.random() * 80 - 40,
+            Math.random() * 400 + offset.z
+            );
+            positions[i * 3] = pos.x;
+            positions[i * 3 + 1] = pos.y;
+            positions[i * 3 + 2] = pos.z;
+        }
+        return positions;
     }
 
     function createColors() {
-      const colors = new Float32Array(10000 * 3);
-      for (let i = 0; i < 10000; i++) {
-        const grey = Math.random();
-        colors[i * 3] = grey;
-        colors[i * 3 + 1] = grey;
-        colors[i * 3 + 2] = grey;
-      }
-      return colors;
+        const colors = new Float32Array(10000 * 3);
+        for (let i = 0; i < 10000; i++) {
+            const grey = Math.random();
+            colors[i * 3] = grey;
+            colors[i * 3 + 1] = grey;
+            colors[i * 3 + 2] = grey;
+        }
+        return colors;
     }
   }
 };
 </script>
   
-  <style scoped>
-  div {
+<style scoped>
+div {
     width: 100%;
     height: 100%;
-  }
-  </style>
+}
+</style>
   
